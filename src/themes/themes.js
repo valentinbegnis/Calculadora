7c14c9277d18ba3b8dd1371dc47c0d9ab3401834
Valentin Begnis <begnisvalentin@gmail.com>
2022-03-01 18:43:21 -0300

    nueva carpeta con los archivos css correspondientes a cada theme

---
+++ src/themes/themes.js
@@ -0,0 +1,21 @@
+const themeSelector = document.getElementById('themeSelector');
+const themeLink = document.getElementById('themeLink');
+
+const themes = ['./themes/theme-1.css',
+ './themes/theme-2.css',
+  './themes/theme-3.css'
+];
+
+themeSelector.addEventListener('click', () => {
+    let hrefValue =  themeLink.getAttribute('href');
+
+    if(hrefValue === themes[0]) {
+        themeLink.setAttribute('href', themes[1]);
+    } else if(hrefValue === themes[1]) {
+        themeLink.setAttribute('href', themes[2]);
+    } else {
+        themeLink.setAttribute('href', themes[0]);
+    }
+});
+
+
